a41b27266c53a0fa5b54db59e6194f263f8d33c3
Nick-Fanelli <nickfanelli5@gmail.com>
2021-09-17 21:23:54 -0400

    chore: migrate from cmake to premake and remove garbage

---
+++ premake5.lua
@@ -0,0 +1,57 @@
+project "NFD"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ( "%{wks.location}/build/" .. outputdir .. "/%{prj.name}")
+    objdir ( "%{wks.location}/build-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "src/include/*.h",
+        "src/include/*.hpp"
+    }
+
+    includedirs {
+        "src",
+        "src/include"
+    }
+
+    filter "system:macosx"
+        cppdialect "C++17"
+        staticruntime "On"
+
+        files {
+            "src/nfd_cocoa.m"
+        }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+        files {
+            "src/nfd_win.cpp"
+        }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+        files {
+            "src/nfd_gtk.cpp"
+        }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+    optimize "on"
+
